6e08fbb381ded7ffc9bf7c88392d353e92aecbd7
Tomasz Pawlowski <tptpawlowski@gmail.com>
2018-01-23 12:19:32 +0100

    reverted cottoncandy memmap into initial state

--- alpenglow/files.py
+++ alpenglow/files.py
@@ -7,24 +7,24 @@ import tempfile
 from PIL import Image
 
 
-def array_to_s3(arr, s3_fname ,cci):
+def array_to_s3(arr, s3_fname, cci):
     """
     Place an array directly into an S3 tiff file
-    
+
     Parameters
     ----------
     arr : 2D numpy array/memmap
-    
+
     s3_fname : str
         Full path to the S3 object to create (e.g., '/test/test.tif'
-    
-    cci : cottoncandy interface object    
+
+    cci : cottoncandy interface object
     """
     # Create a PIL image object:
     im = Image.fromarray(arr)
     # This stands in place of the file to save to:
     output = BytesIO()
-    # Do the saving: 
+    # Do the saving:
     im.save(output, format="tiff")
     # Need to seek back to the beginning of the file:
     output.seek(0)
@@ -33,20 +33,20 @@ def array_to_s3(arr, s3_fname ,cci):
     # Off we go:
     obj.upload_fileobj(output)
 
-    
+
 def s3_to_array(f, cci):
-    """ 
-    Read a tif file straight from an S3 bucket (provided as a cottoncandy 
+    """
+    Read a tif file straight from an S3 bucket (provided as a cottoncandy
     interface) into a numpy array
-   
+
     Parameters
     ----------
     f : str
         The name of the file in the bucket
-    
+
     cci : cottoncandy interface
-    
-    
+
+
     """
     o = cci.download_object(f)
     b = BytesIO(o)
@@ -59,38 +59,37 @@ def read_strip_files(file_list, files_per_strip, ss, cci, dtype, shape):
     """
     From a given list of files read all the tifs in one strip
     and return a memory-mapped array with the data.
-    
+
     Parameters
     ----------
-    file_list : list 
-        All the file names from one experiment, ordered 
-        according to strips 
-    
+    file_list : list
+        All the file names from one experiment, ordered
+        according to strips
+
     files_per_strip : int
         How many files (sheets) in each strip.
-    
+
     ss : int
         A strip index.
-    
+
     cci : a cottoncandy interface
-    
-    Return 
+
+    Return
     ------
     Memory-mapped array with dimensions (z, width, sheets)
     """
-    mm_fd, mm_fname = tempfile.mkstemp(suffix='.memmap')    
-    strip_mm = np.memmap(mm_fname, dtype=dtype, 
+    mm_fd, mm_fname = tempfile.mkstemp(suffix='.memmap')
+    strip_mm = np.memmap(mm_fname, dtype=dtype,
                          shape=(files_per_strip, shape[0], shape[1]))
     for ii in range(files_per_strip):
         image_file = file_list[ss * files_per_strip + ii]
         strip_mm[ii] = s3_to_array(image_file, cci)
-    
+
     mm_roll = np.swapaxes(np.swapaxes(strip_mm, 0, 1), 1, 2)
     # Strips are rastered back and forth, so we flip the odds
     if np.mod(ss, 2):
         return mm_roll[..., ::-1]
     return mm_roll
-
 
 
 def download_s3(remote_fname, local_fname, bucket_name="alpenglowoptics"):
@@ -100,51 +99,52 @@ def download_s3(remote_fname, local_fname, bucket_name="alpenglowoptics"):
     if not os.path.exists(local_fname):
         s3 = boto3.resource('s3')
         b = s3.Bucket(bucket_name)
-        b.download_file(remote_fname, local_fname)    
+        b.download_file(remote_fname, local_fname)
 
-def create_zstack(scan_name, strip_num, z_levels, sample_image_num = 55):
+
+def create_zstack(scan_name, strip_num, z_levels, sample_image_num=55):
     """
     creates a zstack from a series of tiff frames for a single strip
-    
+
     Parameters
     ----------
     scan_name = the label for the scan files (string)
     strip_num = the index of the strip (int)
     z_levels = number of pixels in height dimension (int)
     sample_image_num = arbitrarily chosen frame for reading dtype and shape for each frame (int)
-    
+
     Returns
     -------
     ImageCollection
     """
-  
-    #downloading frames from s3 to ec2 instance
-    for x in range(1, z_levels+1):
-        fname = "%06d_%06d.tif" % (strip_num, x) # XXX we need to enforce 06d, start from 0
-        download_s3('%s/%06d/' %(scan_name, strip_num) + fname, '../data/%s/%06d/' %(scan_name, strip_num) + fname)    
+
+    # downloading frames from s3 to ec2 instance
+    for x in range(1, z_levels + 1):
+        fname = "%06d_%06d.tif" % (strip_num, x)  # XXX we need to enforce 06d, start from 0
+        download_s3('%s/%06d/' % (scan_name, strip_num) + fname, '../data/%s/%06d/' % (scan_name, strip_num) + fname)
 
     imread = delayed(skimage.io.imread, pure=True)  # Lazy version of imread
     filenames = []
     for x in range(0, z_levels):
         fname = "%06d_%06d.tif" % (strip_num, x)
-        filenames.append('../data/%s/%06d/' %(scan_name, strip_num) + fname)
+        filenames.append('../data/%s/%06d/' % (scan_name, strip_num) + fname)
 
-    lazy_values = [imread(filename) for filename in filenames]    
+    lazy_values = [imread(filename) for filename in filenames]
     sample = skimage.io.imread(filenames[sample_image_num])
-    arrays = [da.from_delayed(lazy_value,           # Construct a small Dask array
-                              dtype=sample.dtype,   # for every lazy value
+    arrays = [da.from_delayed(lazy_value,  # Construct a small Dask array
+                              dtype=sample.dtype,  # for every lazy value
                               shape=sample.shape)
               for lazy_value in lazy_values]
 
-    stack = da.stack(arrays, axis=0)  
+    stack = da.stack(arrays, axis=0)
 
-    for z in range(1,stack.shape[1]):
-        filename = 'zstack_%06d_%06d.tif' % (strip_num, z) 
-        tiff.imsave(filename, stack[:,z,:].compute())
-    zstack = ImageCollection('zstack_%06d_*.tif' % strip_num )
+    for z in range(1, stack.shape[1]):
+        filename = 'zstack_%06d_%06d.tif' % (strip_num, z)
+        tiff.imsave(filename, stack[:, z, :].compute())
+    zstack = ImageCollection('zstack_%06d_*.tif' % strip_num)
     return zstack
 
-    
+
 ### DCIMG file i/o:
 
 """
@@ -179,28 +179,28 @@ SESSION_HEADER_BYTES = np.zeros(1, dtype=SESSION_HEADER_DTYPE).nbytes
 
 # newer versions of the dcimg format have a different header
 SESSION_HEADER_DTYPE_INT2P24 = [
-                        ('session_length', 'i4'),
-                        ('pad1', '5i4'),
-                        ('psuedo_offset', 'i4'),
-                        ('pad2', '8i4'),   # there were mysteries number 1,
-                                           # 144, 65537 in padding
-                        ('num_frames', 'i4'),
-                        ('pixel_type', 'i4'),
-                        ('camera_center', 'i4'),
-                        ('num_columns', 'i4'),
-                        ('num_rows', 'i4'),      # num_rows switched position
-                        ('bytes_per_row', 'i4'),
-                        ('bytes_per_image', 'i4'),
-                        ('pad3', '2i4'),
-                        ('offset_to_data', 'i4'),
-                        ('pad4', '4i4'),
-                        ('bytes_per_frame', 'i4')]  # end_of_a_frame =
-                                                    # bytes_per_image + 32
-                                                    # bytes
+    ('session_length', 'i4'),
+    ('pad1', '5i4'),
+    ('psuedo_offset', 'i4'),
+    ('pad2', '8i4'),  # there were mysteries number 1,
+    # 144, 65537 in padding
+    ('num_frames', 'i4'),
+    ('pixel_type', 'i4'),
+    ('camera_center', 'i4'),
+    ('num_columns', 'i4'),
+    ('num_rows', 'i4'),  # num_rows switched position
+    ('bytes_per_row', 'i4'),
+    ('bytes_per_image', 'i4'),
+    ('pad3', '2i4'),
+    ('offset_to_data', 'i4'),
+    ('pad4', '4i4'),
+    ('bytes_per_frame', 'i4')]  # end_of_a_frame =
+# bytes_per_image + 32
+# bytes
 
 SESSION_HEADER_BYTES_INT2P24 = np.zeros(
-                            1,
-                            dtype=SESSION_HEADER_DTYPE_INT2P24).nbytes
+    1,
+    dtype=SESSION_HEADER_DTYPE_INT2P24).nbytes
 
 
 class DCIMGFile(object):
@@ -261,7 +261,6 @@ class DCIMGFile(object):
         info['format_version'] = int(np.fromstring(header[8:12], 'uint32'))
 
         if not (info['format_version'] in [0x7, 0x1000000]):
-
             # in example data this is always 7. Warn if this is not the case,
             # as further assumptions might be invalid
 
@@ -310,12 +309,14 @@ class DCIMGFile(object):
 
         # read the 1st session header  - vormat varies depending on file format
         if info['format_version'] == 7:
-            session_head = np.fromstring(header[info['session0_offset']:(info['session0_offset']+SESSION_HEADER_BYTES)],
-                                        dtype=SESSION_HEADER_DTYPE)
+            session_head = np.fromstring(
+                header[info['session0_offset']:(info['session0_offset'] + SESSION_HEADER_BYTES)],
+                dtype=SESSION_HEADER_DTYPE)
 
         elif info['format_version'] == 0x1000000:
-            session_head = np.fromstring(header[info['session0_offset']:(info['session0_offset']+SESSION_HEADER_BYTES_INT2P24)],
-                                    dtype=SESSION_HEADER_DTYPE_INT2P24)
+            session_head = np.fromstring(
+                header[info['session0_offset']:(info['session0_offset'] + SESSION_HEADER_BYTES_INT2P24)],
+                dtype=SESSION_HEADER_DTYPE_INT2P24)
 
             # each image include data and 32 bytes footer
         else:
@@ -324,15 +325,15 @@ class DCIMGFile(object):
         info['pixel_type'] = session_head['pixel_type']
         info['num_columns'] = session_head['num_columns']
         info['bytes_per_row'] = session_head['bytes_per_row']
-        info['bytes_per_pixel'] = info['bytes_per_row']/info['num_columns']
+        info['bytes_per_pixel'] = info['bytes_per_row'] / info['num_columns']
         info['num_rows'] = session_head['num_rows']
         info['session0_data'] = info['session0_offset'] + session_head['offset_to_data']
 
-        #info['session0_footer'] = info['session0_offset'] + session_head['offset_to_footer']
+        # info['session0_footer'] = info['session0_offset'] + session_head['offset_to_footer']
 
         info['bytes_per_image'] = session_head['bytes_per_image']
         try:
-            #if the bytes per frame is different to the bytes per image (e.g.
+            # if the bytes per frame is different to the bytes per image (e.g.
             # if there is a frame footer)
 
             info['bytes_per_frame'] = session_head['bytes_per_frame']
